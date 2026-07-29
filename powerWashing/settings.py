99a6738c5c1cb47eaa1fb6959195065aef267f08
tufaoydsq <153139678+tufaoydsq@users.noreply.github.com>
2026-07-29 05:11:07 +0200

    Remove duplicate AuthenticationMiddleware entry

--- powerWashing/settings.py
+++ powerWashing/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'core.middleware.AvisoLicencaMiddleware',
 ]
 
 ROOT_URLCONF = 'powerWashing.urls'
